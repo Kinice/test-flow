bf0460c266270a461b7882cd7721e414ca0da280
Kinice <sunzhaopeng.szp@alibaba-inc.com>
2025-09-08 15:34:07 +0800

    t

--- test-monorepo/test.js
+++ test-monorepo/test.js
@@ -1,6 +1,6 @@
 Object.keys(process.env).forEach(key => {
   console.log(key, process.env[key])
 })
-for (let i = 0; i < 2000; i++) {
+for (let i = 0; i < 5000; i++) {
   console.log('我输出了这么多行了：', i);
 }
